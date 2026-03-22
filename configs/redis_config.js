e3e16e38c934bc1d5e65793652fe3de065e0c9b3
Tawan <hatsatorn.narinram@gmail.com>
2026-03-22 22:53:53 +0700

    add env and change connection redis

--- configs/redis_config.js
+++ configs/redis_config.js
@@ -1,10 +1,18 @@
 import { createClient } from "redis";
+import dotenv from "dotenv";
 
+const env = process.env.NODE_ENV.trim() || "development".trim();
+console.log("ENV:", env);
+dotenv.config({
+  path: `.env.${env}`
+});
+
+// console.log("ENV:", env);
+// console.log("REDIS_HOST:", process.env.REDIS_HOST);
+// console.log("REDIS_PASSWORD:", process.env.REDIS_PASSWORD);
+// console.log("REDIS_PORT:", process.env.REDIS_PORT);
 const redisClient = createClient({
-  socket: {
-    host: 'redis',
-    port: 6379,
-  },
+  url: process.env.REDIS_URL,
 });
 
 
